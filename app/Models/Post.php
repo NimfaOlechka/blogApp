9d7f3d1ad7bd3088c65ac2c3054fc5320d889034
Olga K <nimfaolya@mail.ru>
2021-09-08 20:56:10 +0200

    Adding Categories to the posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $guarded = [];
     //protected $guarded = ['id'];// everything except this one
     //protected $fillable = ['title', 'excerpt', 'body', 'slug']; // allowed to be mass assigned
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
 }
