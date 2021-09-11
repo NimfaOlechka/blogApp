f6a76ef6664fe58215225052b507a33b9c2e9395
Olga K <nimfaolya@mail.ru>
2021-09-11 18:57:01 +0200

    category added as eloquent constraint

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,14 +18,24 @@ class Post extends Model
             $query
                 ->where('title', 'like', '%'.$search.'%')
                 ->orWhere('body', 'like', '%'.$search.'%'));
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>       
+            $query->whereHas('category', fn($query) => $query->where('slug', $category))
+        );
         
+        /* $query->when($filters['category'] ?? false, fn($query, $category) =>       
+            $query
+            ->whereExists((fn($query) =>
+                $query->from('categories')
+                ->whereColumn('categories.id','posts.category_id')
+                ->where('categories.slug', $category))
+            )); */
 
         /* if($filters['search'] ?? false){
             $query
                 ->where('title', 'like', '%'.request('search').'%')
                 ->orWhere('body', 'like', '%'.request('search').'%');
-        } */
-        //return $query;
+        } */        
     }
 
     public function category()
--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -9,7 +9,7 @@
             <x-dropdown>
                 <x-slot name="trigger">
                     <button  class="py-2 pl-3 pr-9d text-sm text-left font-semibold w-full lg:w-32 inline-flex">
-                        {{ isset($currentCategory)? ucwords($currentCategory->name) : 'Categories' }}
+                        {{ isset($currentCategory)? ucwords($currentCategory->name) : 'Categories' }}                        
                         <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
                     </button>
                 </x-slot>
@@ -19,7 +19,7 @@
                 {{-- :active = 'request()->is("/categories/{{$category->slug}}")'  - another way of the check--}}
                 @foreach ($categories as $category)               
                     <x-dropdown-item 
-                        href="/categories/{{$category->slug}}"
+                        href="/?category={{$category->slug}}"
                         :active="isset($currentCategory) && $currentCategory->is($category)"                        
                         >{{ucwords($category->name)}}
                     </x-dropdown-item>                    
