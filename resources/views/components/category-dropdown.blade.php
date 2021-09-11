563ce7950a8109d956989c5540fd93e62cf1ba0d
Olga K <nimfaolya@mail.ru>
2021-09-11 21:09:46 +0200

    Searchin within the given category

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -12,7 +12,7 @@
     {{-- :active = 'request()->is("/categories/{{$category->slug}}")'  - another way of the check--}}
     @foreach ($categories as $category)               
         <x-dropdown-item 
-            href="/?category={{$category->slug}}"
+            href="/?category={{$category->slug}}&{{ http_build_query(request()->except('category')) }}"
             :active="isset($currentCategory) && $currentCategory->is($category)"                        
             >{{ucwords($category->name)}}
         </x-dropdown-item>                    
