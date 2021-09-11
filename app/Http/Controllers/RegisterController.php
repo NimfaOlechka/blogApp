c4e2eb5657aaed88cf1ff55ef0d161998527e5fc
Olga K <nimfaolya@mail.ru>
2021-09-11 23:33:10 +0200

    Failed VAlidation n Old Input Data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,9 +17,9 @@ class RegisterController extends Controller
     {
         //var_dump(request()->all());
         $attributes = request()->validate([
-            'name' => 'required',
-            'username' => 'required|max:255',
-            'email' => 'required|email',
+            'name' => 'required|max:255',
+            'username' => 'required|max:255|unique:users,username',
+            'email' => 'required|email|unique:users,email',
             'password' => ['required', 'min:7', 'max:255']
         ]);       
         
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,8 +16,13 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name')}}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-s mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -31,8 +36,12 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username')}}"
                            required
                     >
+                    @error('username')
+                        <p class="text-red-500 text-s mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -46,8 +55,12 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email')}}"
                            required
                     >
+                    @error('email')
+                        <p class="text-red-500 text-s mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -63,6 +76,9 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                        <p class="text-red-500 text-s mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -72,6 +88,14 @@
                         Submit
                     </button>
                 </div>
+
+                @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs mt-1">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             </form>
         </main>
     </section>
